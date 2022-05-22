5a371745fe2972870ec515e18c68ce19f48aca14
cjn-worker <2387204738@qq.com>
2022-05-23 00:46:30 +0800

    修复Shf功能

--- src/t1lp/calculator/Operator.java
+++ src/t1lp/calculator/Operator.java
@@ -1,20 +1,24 @@
 package t1lp.calculator;
 
+import static t1lp.handle.Config.Log;
+
 abstract class Operator {
-    boolean judge=false;
+    boolean judge=false;//辨别传回数值形式的标识符
     char True = '1';
-    String str="00000000000000000000000000000000";
-    protected double X;
-    protected double Y;
-    protected double Z;
-    protected String Xb;
-    protected String Yb;
-    protected String Zb="";
+    String str="00000000000000000000000000000000";//32个字节的字符串，用于移位后的补0
+    protected double X;//第一个操作数
+    protected double Y;//第二个操作数
+    protected double Z;//以double形式反馈的结果
+    protected String Xb;//第一个操作数的二进制形式
+    protected String Yb;//第二个操作数的二进制形式
+    protected String Zb="";//以string形式返回的结果
     public Operator(MyNumber x,MyNumber y){
         X=Double.parseDouble(x.toString(10).substring(5));
         Y=Double.parseDouble(y.toString(10).substring(5));
         Xb=x.toString(2).substring(5);
         Yb=y.toString(2).substring(5);
+        Log("Operator","Operator(MyNumber x:"+x+",MyNumber y:"+y+")","X:"+X+"\tY:"+Y+"\tXb:"+Xb+"\tYb:"+Yb);
+        Log("Operator","getResult()","Z:"+Z+"\tZb:"+Zb);
         doCalculate();
     }
     protected abstract void doCalculate();
@@ -26,6 +30,11 @@ abstract class Operator {
     }
 }
 
+/**
+ * 各种运算
+ *
+ * @author cjn——worker
+ */
 class Add extends Operator{
 
     public Add(MyNumber x, MyNumber y) {
@@ -130,9 +139,9 @@ class Shf extends Operator{
     protected void doCalculate(){
         int change = (int)(Y);
         if(change>0) {
-            String newStr=Xb+str;
-            for (int i = 0; i < Xb.length(); i++) {
-                Zb += newStr.charAt(i + change);
+            String newStr=str+Xb+str;
+            for (int i = Xb.length()+change-1; i < 33+Xb.length()+change-1; i++) {
+                Zb += newStr.charAt(i);
             }
         }
         else{
